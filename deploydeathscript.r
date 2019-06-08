c77f29a7025b40a2e68246f6bf5d4947c78a2234
Wilks <awilks@rand.org>
2019-06-08 11:34:21 -0700

    update path

--- deploydeathscript.r
+++ deploydeathscript.r
@@ -17,3 +17,18 @@ library(caret)
 
 rsconnect::deployApp(appDir = "./predict_death_api")
 
+predict.death.rate <- function(
+  HAS_CHARTEVENTS_DATA=NA, #1/0
+  ADMISSION_TYPE=NA,       #ELECTIVE / EMERGENCY / NEWBORN / URGENT
+  MARITAL_STATUS=NA,       #UNK / DIVORCED / LIFE.PARTNER / MARRIED / SEPARATED / SINGLE / WIDOWED
+  ADMISSION_LOCATION=NA,   #UNK / EMERGENCY_ROOM /TRANSFER_WITHIN
+  INSURANCE=NA,            #GOVERNMENT / MEDICAID / MEDICARE / PRIVATE / SELFPAY
+  ETHNICITY=NA,            #WHITE / BLACK / ASIAN / LATINO
+  LANGUAGE=NA,             #UNK / ENGLISH / SPANISH / KOREAN
+  RELIGION=NA,             #UNK / CATHOLIC / JEWISH / MUSLIM
+  DX1=NA,                  #"DX___"
+  DX2=NA,                  #"DX___"
+  DX3=NA,                  #"DX___"
+  DX4=NA,                  #"DX___"
+  DX5=NA                   #"DX___"
+)
